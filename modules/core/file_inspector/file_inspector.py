f531c4947a4b3e27e1d40678a326704584ce1ab8
lheido <leonard.treille@gmail.com>
2015-10-09 09:45:24 +0200

    locale change

--- modules/core/file_inspector/file_inspector.py
+++ modules/core/file_inspector/file_inspector.py
@@ -35,11 +35,12 @@ def connect_widgets(vertical_widgets, horizontal_widgets):
 def on_file_saved(editor):
     file_info = editor.file_info
     file = FileInspectorHelper.query(File).\
-        filter(File.path == file_info.absoluteFilePath()).one()
-    file = FileInspectorHelper.update_file(file, file_info, True)
-    editor.parentWidget().parentWidget().parentWidget().parentWidget().\
-        parentWidget().parentWidget().horizontal_widgets['file_inspector'].\
-        populate(None, file)
+        filter(File.path == file_info.absoluteFilePath()).first()
+    if file:
+        file = FileInspectorHelper.update_file(file, file_info, True)
+        editor.parentWidget().parentWidget().parentWidget().parentWidget().\
+            parentWidget().parentWidget().horizontal_widgets['file_inspector'].\
+            populate(None, file)
 
 
 class FileInspector(QTreeView):
